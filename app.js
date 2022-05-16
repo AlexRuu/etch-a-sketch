263b88aae6eaa3000af174237bd068c7178b4896
Alex <weiru@hotmail.ca>
2022-05-16 18:32:13 -0400

    Update grid and implement colour selector

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Global variable declaration
 const container = document.querySelector('.grid-container');
+let colour = document.querySelector('.colour');
+let drawBrush = document.querySelector('.draw-brush');
+let eraseBrush = document.querySelector('.erase-brush');
 
-// Make the grid
+/* Make the grid
 function makeGrid(row, col) {
     for (i = 0; i < row * col; i++) {
         let square = document.createElement('div')
@@ -9,26 +12,46 @@ function makeGrid(row, col) {
         container.appendChild(square);
     }
 }
+*/
 
-makeGrid(16, 16);
+drawBrush.addEventListener('click', draw);
+eraseBrush.addEventListener('click', erase);
+makeGrid(10);
+
+
+colour.onchange = function() {
+    colour = this.value;
+    return colour;
+}
+
+// Functions
+// Grid function
+function makeGrid(value) {
+    container.style.gridTemplateColumns = `repeat(${value}, 1fr)`
+    container.style.gridTemplaterows = `repeat(${value}, 1fr)`
+    
+    for (i = 0; i < value * value; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    };
+};
+
 
 // Get squares div variable for brushes
 const blocks = document.querySelectorAll('.square');
 
 // Draw function 
-
 function draw() {
     blocks.forEach((div) => {
         div.addEventListener('mousemove', function(event) {
             if (event.buttons === 1) {
-                div.setAttribute('style', 'background-color: black;')
+                console.log(colour);
+                div.setAttribute('style', `background-color: ${colour};`)
             };
         });
     });
 };
-
-let drawBrush = document.querySelector('.draw-brush');
-drawBrush.addEventListener('click', draw);
 
 // Erase function 
 function erase () {
@@ -41,5 +64,3 @@ function erase () {
     });
 };
 
-let eraseBrush = document.querySelector('.erase-brush');
-eraseBrush.addEventListener('click', erase);
